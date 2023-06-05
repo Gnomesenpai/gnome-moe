520743b64e52623a00ccb558d54cb583e8df73a2
gnomesenpai <toshua_4_eve@hotmail.co.uk>
2023-06-05 21:41:39 +0100

    yup

--- website/index.php
+++ website/index.php
@@ -3,9 +3,12 @@
 <?php 
 $var= "testing"; 
 ?>
-<center><img src="./img/hackerman.jpg" style="max-width: 100%; " /></center>
+<!--<center><img src="./img/hackerman.jpg" style="max-width: 100%; " /></center>-->
 <br>
-<center><a href="https://github.com/Gnomesenpai/hackerman">Github repo</a></center>
+<br>
+<p>I will do something with this, eventually...</p>
+<br>
+<center><a href="https://github.com/Gnomesenpai/gnome-moe">Github repo</a></center>
 
 </html>
 
