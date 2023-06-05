8f333de42d1499c8a236bae448c0952e90944a16
gnomesenpai <toshua_4_eve@hotmail.co.uk>
2023-06-05 22:48:40 +0100

    center shit

--- website/index.php
+++ website/index.php
@@ -6,7 +6,7 @@ $var= "testing";
 <!--<center><img src="./img/hackerman.jpg" style="max-width: 100%; " /></center>-->
 <br>
 <br>
-<p>I will do something with this, eventually...</p>
+<center><p>I will do something with this, eventually...</p></center>
 <br>
 <center><a href="https://github.com/Gnomesenpai/gnome-moe">Github repo</a></center>
 
